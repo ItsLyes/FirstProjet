fffd564b6be6f28e6a4bc0dd31a20732b03f3e4b
lyes <lyes.hlrs@gmail.com>
2023-03-22 18:13:29 -0400

    add some main code

--- Project.cpp
+++ Project.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 
-    int main()
-    {
-        
-
-
-    }
+int main() {
+	float num1, num2;
+	char op;
+	 std::cout<<"please type your number : \n";
+	 std::cin>>num1;
+	 if(std::cin.fail()){
+		 std::cout<<"please enter a valid number\n";
+		}
+	 else{
+		 std::cout<<"you choose :"<<num1<< std::endl;
+	 }
+}
