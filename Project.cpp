e5a1a974c6ff2a4409ad597fda9617ffa09fa8bf
lyes <lyes.hlrs@gmail.com>
2023-03-22 19:42:20 -0400

    calculator is now working

--- Project.cpp
+++ Project.cpp
@@ -4,6 +4,8 @@
 int main() {
 	float num1, num2;
 	char op;
+    std::cout<<"*************************"<<std::endl;
+     std::cout<<"Welcome to calculator"<<std::endl;
 	 std::cout<<"please type your number : \n";
 	 std::cin>>num1;
 	 if(std::cin.fail()){
@@ -18,12 +20,29 @@ int main() {
 		 std::cout<<"please enter a valid number\n";
 		}
 	 else{
-		 std::cout<<"you choosed :"<<num2<< std::endl;
+		 std::cout<<"you choosed : "<<num2<< std::endl;
 	 }
-     std::cout<<"enter an operator :"<<std::endl;
+     std::cout<<"enter an operator ( x , / , + , - ):"<<std::endl;
+     std::cin>>op;
      op = std::tolower(op);
-    
-     
-
-    
+    if(op=='x'){
+        std::cout<<"you choosed to do "<<num1<<" "<<op<<" "<<num2<<std::endl;
+        std::cout<<"your result is : "<<num1*num2<<std::endl;
+        std::cout<<"*************************"<<std::endl;
+    }
+    if(op=='/'){
+        std::cout<<"you choosed to do "<<num1<<" "<<op<<" "<<num2<<std::endl;
+        std::cout<<"your result is : "<<num1/num2<<std::endl;
+        std::cout<<"*************************"<<std::endl;
+    }
+    if(op=='+'){
+        std::cout<<"you choosed to do "<<num1<<" "<<op<<" "<<num2<<std::endl;
+        std::cout<<"your result is : "<<num1+num2<<std::endl;
+        std::cout<<"*************************"<<std::endl;
+    }
+    if(op=='-'){
+        std::cout<<"you choosed to do "<<num1<<" "<<op<<" "<<num2<<std::endl;
+        std::cout<<"your result is : "<<num1-num2<<std::endl;
+        std::cout<<"*************************"<<std::endl;
+    }
 }
