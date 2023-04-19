72b54918f7844c38be22f91e304a64071d89d945
lyes <lyes.hlrs@gmail.com>
2023-04-19 11:10:43 -0400

    enlever using namespace std

--- Main.cpp
+++ Main.cpp
@@ -2,26 +2,25 @@
 #include <iostream>
 #include <string>
 #include <vector>
-using namespace std;
 
 struct Task {
-    string name;
-    string due_date;
+    std::string name;
+    std::string due_date;
 };
 
 int main() {
-    vector<Task> tasks;
+    std::vector<Task> tasks;
 
-    cout << "Ajouter une tâche : " << endl;
+    std::cout << "Ajouter une tâche : " << std::endl;
     Task new_task;
-    cout << "Nom de la tâche : ";
-    cin >> new_task.name;
-    cout << "Date d'échéance (jj/mm/aaaa) : ";
-    cin >> new_task.due_date;
+    std::cout << "Nom de la tâche : ";
+    std::cin >> new_task.name;
+    std::cout << "Date d'échéance (jj/mm/aaaa) : ";
+    std::cin >> new_task.due_date;
 
     tasks.push_back(new_task);
 
-    cout << "Tâche ajoutée avec succès !" << endl;
+    std::cout << "Tâche ajoutée avec succès !" << std::endl;
 
     return 0;
 }
